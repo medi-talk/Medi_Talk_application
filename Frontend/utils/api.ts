2b833852f9c820987ab34402641db725139b1ec2
mgyuhw <mgyuhw@gmail.com>
2025-10-21 11:12:30 +0900

    FIX :  서버 접속 가능하게 변경 노션 클론 후 Backend 할 일 API 관련 문서 참고

--- Frontend/utils/api.ts
+++ Frontend/utils/api.ts
@@ -1,11 +1,23 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
+import { BACKEND_APP_CONFIG } from "../config/apiConfig";
+
+const SERVER_BASE = BACKEND_APP_CONFIG.BASE_URL;
+
+// 환경 스위치
+const USE_REMOTE_SERVER = true; // true → 서버로, false → 로컬(adb reverse)
 
 const getBaseURL = () => {
-  return Platform.OS === 'android'
-    ? 'http://localhost:3000'   // USB + adb reverse
-    : 'http://localhost:3000';
-  // 에뮬레이터라면 'http://10.0.2.2:3000'
+   if (!USE_REMOTE_SERVER) {
+    // 개발용
+    return Platform.OS === 'android'
+      ? 'http://localhost:3000'   // USB + adb reverse
+      : 'http://localhost:3000';
+    // 에뮬레이터라면 'http://10.0.2.2:3000'
+   } else {
+    // 서버용
+    return SERVER_BASE;
+  }
 };
 
 const api = axios.create({
